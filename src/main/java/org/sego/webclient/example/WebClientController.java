459d0d176133dea2b3b05a607ce2c9846e93b55c
sego1113 <sego1113@WSMNN-044.netcracker.com>
2017-11-10 16:41:16 +0300

    Supplier mono

--- src/main/java/org/sego/webclient/example/WebClientController.java
+++ src/main/java/org/sego/webclient/example/WebClientController.java
@@ -1,5 +1,12 @@
 package org.sego.webclient.example;
 
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Supplier;
+
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,25 +21,49 @@ public class WebClientController {
 
 	@Autowired
 	protected WebClient webClient;
-	
+
 	// Don't works. Freeze
 	@GetMapping(path = "/people1/{id}")
 	public String getPeopleBlock(@PathVariable Long id) {
 		System.out.println("People block request: " + id);
 		return getPeopleRsp(id).block();
 	}
-	
+
 	// Works
 	@GetMapping(path = "/people2/{id}")
 	public Mono<String> getPeople(@PathVariable Long id) {
 		System.out.println("People request: " + id);
 		return getPeopleRsp(id);
 	}
-	
+
+	// Works
+	@GetMapping(path = "/hello1")
+	public String getHelloBlock() {
+		System.out.println("Hello block request");
+		return getHelloRsp().block();
+	}
+
+	// Works
+	@GetMapping(path = "/hello2")
+	public Mono<String> getHello() {
+		System.out.println("Hello request");
+		return getHelloRsp();
+	}
+
 	private Mono<String> getPeopleRsp(Long id) {
 		String url = "https://swapi.co/api/people/" + id + "?format=json";
-		return webClient.get().uri(url).accept(MediaType.APPLICATION_JSON_UTF8)
-				.header("User-Agent", "Chrome").exchange().log().flatMap(r -> r.bodyToMono(String.class));
+		return webClient.get().uri(url).accept(MediaType.APPLICATION_JSON_UTF8).header("User-Agent", "Chrome")
+				.exchange().log().flatMap(r -> r.bodyToMono(String.class));
+	}
+
+	private Mono<String> getHelloRsp() {
+		return Mono.fromSupplier(new Supplier<String>() {
+			@Override
+			public String get() {
+				// TODO Auto-generated method stub
+				return "Hello";
+			}
+		}).delayElement(Duration.ofSeconds(2));
 	}
 
 }
